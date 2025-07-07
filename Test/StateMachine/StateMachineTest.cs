6c841e9a99b581252708d4900e8a1996895c8e07
John Lomba <johnlomba1993@gmail.com>
2025-07-07 12:27:54 -0400

    added some unit test

---
+++ Test/StateMachine/StateMachineTest.cs
@@ -0,0 +1,35 @@
+﻿using LombdaAgentSDK.StateMachine;
+
+namespace Test
+{
+    public class StateMachineTest
+    {
+        public class MockState : BaseState<string, int>
+        {
+            public override async Task<int> Invoke()
+            {
+                Assert.That(Input.GetType(), Is.EqualTo(typeof(string)));
+                Output = 3;
+                return Output;
+            }
+        }
+
+        [Test]
+        public async Task TestStateTransition()
+        {
+            MockState state1 = new MockState();
+
+            state1.Input = "test input";
+
+            int result = 0;
+
+            state1.Transitions?.Add(new StateTransition<int>((output) => { Console.WriteLine($"Result was {output}"); result = output; return true; }, new MockState()));
+
+            StateMachine stateMachine = new();
+
+            stateMachine.Run(state1);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
---
+++ Test/Utility/TestUtilityItems.cs
@@ -0,0 +1,47 @@
+﻿using LombdaAgentSDK;
+using LombdaAgentSDK.Agents;
+
+namespace Test.Utility
+{
+    internal class TestUtilityItems
+    {
+        public class Animal
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        [Test]
+        public void TestJsonFormatSchemaCreationOnClass()
+        {
+            ModelOutputFormat modelFormat = json_util.CreateJsonSchemaFormatFromType(typeof(Animal), true);
+            Assert.That(modelFormat.JsonSchemaFormatName, Is.EqualTo("Animal"));
+            Assert.That(modelFormat.JsonSchema, Is.Not.Null);
+            Assert.That(modelFormat.JsonSchemaIsStrict, Is.True);
+        }
+
+        public struct AnimalStruct
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        [Test]
+        public void TestJsonFormatSchemaCreationOnStruct()
+        {
+            ModelOutputFormat modelFormat = json_util.CreateJsonSchemaFormatFromType(typeof(AnimalStruct), true);
+            Assert.That(modelFormat.JsonSchemaFormatName, Is.EqualTo("Animal"));
+            Assert.That(modelFormat.JsonSchema, Is.Not.Null);
+            Assert.That(modelFormat.JsonSchemaIsStrict, Is.True);
+        }
+
+        [Test]
+        public void TestJsonParser()
+        {
+            string json = "{\"Name\":\"Dog\",\"Age\":5}";
+            Animal animal = json.ParseJson<Animal>();
+            Assert.That(animal.Name, Is.EqualTo("Dog"));
+            Assert.That(animal.Age, Is.EqualTo(5));
+        }
+    }
+}
