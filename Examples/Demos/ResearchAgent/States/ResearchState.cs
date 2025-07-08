178b1062cbd2ce3b67916ef60207f508d121d732
John Lomba <johnlomba1993@gmail.com>
2025-07-08 10:51:46 -0400

    added concurrency to ResearchAgent

--- Examples/Demos/ResearchAgent/States/ResearchState.cs
+++ Examples/Demos/ResearchAgent/States/ResearchState.cs
@@ -8,12 +8,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using LlmTornado.Responses;
 
 namespace Examples.Demos.ResearchAgent.States
 {
     class ResearchState : BaseState<WebSearchPlan, string>
     {
+        bool RunParallel { get; set; } = false;
+        public ResearchState(bool runParallel = false) { RunParallel = runParallel; }
+
         public override async Task<string> Invoke()
+        {
+            Console.WriteLine("[Starting WebSearch from StateMachine]");
+
+            return RunParallel ? await InvokeParallel() : await InvokeThreaded();
+        }
+
+        public async Task<RunResult> RunResearchAgent(WebSearchItem item)
         {
             string instructions = """
                     You are a research assistant. Given a search term, you search the web for that term and
@@ -22,21 +34,32 @@ namespace Examples.Demos.ResearchAgent.States
                     grammar. This will be consumed by someone synthesizing a report, so its vital you capture the 
                     essence and ignore any fluff. Do not include any additional commentary other than the summary itself.
                     """;
-
             Agent agent = new Agent(new OpenAIModelClient("gpt-4o-mini", enableWebSearch: true), "Search agent", instructions);
+            return await Runner.RunAsync(agent, item.query);
+        }
 
-            List<string> searchResults = new List<string>();
+        public async Task<string> InvokeParallel()
+        {
+            ConcurrentBag<RunResult> researchResults = new ConcurrentBag<RunResult>();
 
-            Console.WriteLine("[Starting WebSearch from StateMachine]");
+            await Parallel.ForEachAsync(
+                Input.items.AsEnumerable(),
+                async (item, CancellationToken) => researchResults.Add((await RunResearchAgent(item))));
 
-            foreach (WebSearchItem item in this.Input.items)
-            {
-                RunResult result = await Runner.RunAsync(agent, item.query);
+            return string.Join("[RESEARCH RESULT]\n\n\n", researchResults.ToList().Select(result => result.Text));
+        }
 
-                searchResults.Add(result.Text ?? "");
-            }
+        public async Task<string> InvokeThreaded()
+        {
+            List<Task<RunResult>> researchTask = new List<Task<RunResult>>();
+            
+            Input.items.ToList()
+                .ForEach(item =>
+                    researchTask.Add(Task.Run(async () => await RunResearchAgent(item))));
 
-            return string.Join("[SEARCH RESULT]\n\n\n", searchResults);
+            RunResult[] researchResults = await Task.WhenAll(researchTask);
+
+            return string.Join("[RESEARCH RESULT]\n\n\n", researchResults.ToList().Select(result => result.Text));
         }
     }
 }
